bfc3608a801100fe1cbe20aa2d7b48cf42e55389
yemmieh <yemioyetoro@gmail.com>
2020-11-09 22:31:30 -0600

    added crud and swagger to post.js

--- routes/posts.js
+++ routes/posts.js
@@ -32,13 +32,121 @@ router.get('/post',(req,res) =>{
 
 /**
  * @swagger
- * /posts/:
- *  post:
- *      description: mongodb post
+ * /posts/{postId}:
+ *  parameters:
+ *      - in: path
+ *        name: postId
+ *        type: string
+ *        required: true
+ *        description: pass postId to parameter
+ *  get:
+ *      description: get mongodb data by Id
  *      responses:
  *          '200':
  *              description: mongodb
  */
+router.get('/:postId',async (req,res) =>{
+    try{
+        console.log(req.params)
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    }catch(err){
+        res.json({errormsg:err})
+    }
+})
+
+
+
+/**
+ * @swagger
+ * /posts/{postId}:
+ *  parameters:
+ *      - in: path
+ *        name: postId
+ *        type: string
+ *        required: true
+ *        description: pass postId to parameter
+ *  delete:
+ *      description: delete mongodb data by Id
+ *      responses:
+ *          '200':
+ *              description: mongodb
+ */
+router.delete('/:postId',async (req,res) =>{
+    try{
+        console.log(req.params)
+        const removePost = await Post.remove({_id:req.params.postId});
+        res.json(removePost);
+    }catch(err){
+        res.json({errormsg:err})
+    }
+})
+
+
+
+/**
+* @swagger
+*  /posts/{postId}:
+*    patch:
+*      summary: update mondo db data, takes parameter in path and body.
+*      consumes:
+*        - application/json
+*      tags:
+*        - update a Db data
+*      parameters:
+*        - in: body
+*          name: user
+*          description: update mondo db data.
+*          schema:
+*            type: object
+*            required:
+*              - title
+*            properties:
+*              title:
+*                type: string
+*      responses:
+*        201:
+*          description: mongodb data updated!
+*/
+router.patch('/:postId',async (req,res) =>{
+    try{
+        console.log(req.params)
+        console.log(req.body)
+        const updatePost = await Post.updateOne({_id:req.params.postId},
+            {$set:{title: req.body.title}});
+        res.json(updatePost);
+    }catch(err){
+        res.json({errormsg:err})
+    }
+})
+
+/**
+* @swagger
+*  /posts/:
+*    post:
+*      summary: create mondo db data.
+*      consumes:
+*        - application/json
+*      tags:
+*        - Create a new db data
+*      parameters:
+*        - in: body
+*          name: user
+*          description: add mondo db data.
+*          schema:
+*            type: object
+*            required:
+*              - description
+*              - title
+*            properties:
+*              description:
+*                type: string
+*              title:
+*                type: string
+*      responses:
+*        201:
+*          description: mongodb data created!
+*/
 router.post('/',(req,res) => 
 {
     const post = new Post({
